a26c878c32e8a02dcdfa583870a37184d066a90b
Dmitry Buzdin <buzdin@gmail.com>
2016-12-11 20:08:26 +0200

    Initial project structure

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        while (1 < 2) {
+            System.out.println("Hello World!");
+            Thread.sleep(1000);
+        }
+    }
+
+}
