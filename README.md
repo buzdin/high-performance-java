b5cc58edbc6be4ccda794a30720054041c34ed30
Dmitry Buzdin <buzdin@gmail.com>
2016-12-11 21:27:40 +0200

    Added JMH

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+# References
+
+* [Introduction to G1](http://www.oracle.com/technetwork/tutorials/tutorials-1876574.html)
+* [VisualVM](https://visualvm.github.io/)
+* [JConsole](http://openjdk.java.net/tools/svc/jconsole/)
+* [JMH Tool](http://hg.openjdk.java.net/code-tools/jmh)
+* [JMH Gradle Plugin./ga](https://github.com/melix/jmh-gradle-plugin)
---
+++ src/jmh/java/lv/buzdin/hpj/HelloJMH.java
@@ -0,0 +1,25 @@
+package lv.buzdin.hpj;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class HelloJMH {
+
+    @Benchmark
+    public void wellHelloThere() {
+        // this method was intentionally left blank.
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(HelloJMH.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
